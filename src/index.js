ce967d170e6c8e0fe8debe59f0aab6b8f9dfae82
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-10-01 21:21:32 +0300

    add html-element

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import * as d3 from 'd3';
 import './style.css';
 
-const KICKSTARTER_URL = "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/kickstarter-funding-data.json";
-const MOVIES_URL = "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json";
-const VIDEOGAMES_URL = "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json";
+const urls = [
+    "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/kickstarter-funding-data.json",
+    "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json",
+    "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json"
+];
 
-
-const tooltip = d3.select("#bar-chart")
+const main = d3.select("body")
     .append("div")
+    .attr("id", "main")
+    .append("span")
+    .attr("id", "title")
+    .text("United States Educational Attainment")
+    .append("br");
+
+main.append("span")
+    .attr("id", "description")
+    .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)");
+
+main.append("g")
+    .attr("class", "a-group")
+    .append("a")
+    .attr("href", urls[0])
+    .text("Kickstarter Dataset");
+main.append("a")
+    .attr("href", urls[1])
+    .text("Movies Dataset");
+main.append("a")
+    .attr("href", urls[2])
+    .text("Video Games Dataset");
+
+const tooltip = main.append("div")
     .attr("id", "tooltip")
     .style("opacity", 0);
