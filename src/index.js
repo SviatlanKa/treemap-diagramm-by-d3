7e2bdfb667bdf2c7a4935c4fd793650c4075caff
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-10-02 22:19:01 +0300

    temp

--- src/index.js
+++ src/index.js
@@ -28,6 +28,13 @@ const dataset = [
     }
 ];
 
+const myColor = [
+    '#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c',
+    '#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928',
+    '#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462',
+    '#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'
+];
+
 const main = d3.select("body")
     .append("div")
     .attr("id", "main");
@@ -79,18 +86,61 @@ d3.json(urls[0]).then(kickRsp => {
                 .append("svg")
                 .attr("viewBox", [0, 0, width, height])
 
-            let root = d3.hierarchy(data);
-            console.log(root)
+            let root = d3.hierarchy(data)
+                .sum(d => d.values)
+                .sort((a, b) => b.height - a.height || b.value - a.value);
+console.log(root.leaves())
+            const colorScale = d3.scaleOrdinal()
+                .domain([1, root.children.length])
+                .range(myColor);
+
             let treemap = d3.treemap();
-            console.log(treemap)
+
             let nodes = treemap
                 .tile(d3.treemapSquarify)
-                .size(width, height)
+                .size([width, height])
                 .padding(1)
-                (root
-                .sum(d => d.values)
-                .sort((a, b) => b.height - a.height || b.value - a.value));
-            console.log(nodes)
+                .round(true)
+                (root);
+
+            // svg.selectAll("rect")
+            //     .data(root.leaves())
+            //     .enter()
+            //     .append("rect")
+            //     .attr("x", d => d.x0)
+            //     .attr("y", d => d.y0)
+            //     .attr("width", d => d.x1 - d.x0)
+            //     .attr("height", d=> d.y1 - d.y0)
+            //     .attr("stroke", 1 + "px")
+            //     // .style("fill", d=> colorScale(d.parent.data.name))
+            // console.log(nodes)
+            const node = svg.datum(root).selectAll(".node")
+                .data(root.leaves())
+                .enter().append("rect")
+                .attr("class", "node")
+                .style("left", (d) => d.x0 + "px")
+                .style("top", (d) => d.y0 + "px")
+                .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
+                .style("height", (d) => Math.max(0, d.y1 - d.y0  - 1) + "px")
+                .style("background", (d) => colorScale(d.parent.data.name))
+                .text((d) => d.data.name);
+
+            d3.selectAll("input").on("change", function change() {
+                const value = this.value === "count"
+                    ? (d) => { return d.size ? 1 : 0;}
+                    : (d) => { return d.size; };
+
+                const newRoot = d3.hierarchy(data, (d) => d.children)
+                    .sum(value);
+
+                node.data(treemap(newRoot).leaves())
+                    .transition()
+                    .duration(1500)
+                    .style("left", (d) => d.x0 + "px")
+                    .style("top", (d) => d.y0 + "px")
+                    .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
+                    .style("height", (d) => Math.max(0, d.y1 - d.y0  - 1) + "px")
+            });
         })
 
     })
